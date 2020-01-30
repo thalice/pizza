8669b4640e6c8c38e0e99205cc2c996fc181d599
Alice Thammavong <thammavongalice@gmail.com>
2020-01-30 11:41:06 +0100

    Add links icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import "./styles/Footer.scss";
 
 function Footer() {
   return (
-    <div>
-      <ul>
-        <li>BloBlo</li>
-        <li></li>
+    <div id="footer">
+      <ul className="links">
+        <li>Qui sommes-nous?</li>
+        <li>Nous contacter</li>
+      </ul>
+      <ul className="icons">
+        <li>
+          <img src="/images/iconinsta.png" alt=""></img>
+        </li>
+        <li>
+          <img src="/images/iconface.png" alt=""></img>
+        </li>
       </ul>
     </div>
   );
