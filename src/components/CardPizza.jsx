2de81943da7a9ec2b88973e5257f1163f66dd2d8
Alice Thammavong <thammavongalice@gmail.com>
2020-01-30 15:55:45 +0100

    Call pizzacards

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledPizzaCard = styled.li`
+  border: 1px solid black;
+  width: 50%;
+
+  h1 {
+    font-weight: bold;
+  }
+`;
 
 function CardPizza({ pizza }) {
+  const { title, ingredients, image } = pizza;
   return (
-    <div>
-      <h1>{}</h1>
-      <ul></ul>
-    </div>
+    <StyledPizzaCard>
+      <h1>{title}</h1>
+      <ul>
+        <li>
+          {ingredients.map(i => {
+            return <li>{i}</li>;
+          })}
+        </li>
+      </ul>
+    </StyledPizzaCard>
   );
 }
 
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import db from "../fakeDB";
+import CardPizza from "../components/CardPizza";
 
 function Menu() {
-  return <div>Quoi manger</div>;
+  return (
+    <div>
+      <h1>Quoi manger</h1>
+      <ul>
+        {db.map(pizza => {
+          return <CardPizza key={pizza.id} pizza={pizza} />;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Menu;
