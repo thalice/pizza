f89882e0317dfd072dffd0b81e4958a0a53a2ebd
Alice Thammavong <thammavongalice@gmail.com>
2020-01-30 15:31:16 +0100

    Create fake DB.js

---
+++ src/fakeDB.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    title: "Leonard",
+    category: "tomate",
+    ingredients: ["base", "tomate", "oignon", "champignon"],
+    image: "/images/pizzaUne.jpg"
+  },
+  {
+    id: 2,
+    title: "Donatello",
+    category: "tomate",
+    ingredients: ["base", "tomate", "jambon", "oignon"],
+    image: "/images/pizzaDeux.jpg"
+  },
+  {
+    id: 3,
+    title: "Micheal-Angelo",
+    category: "blanche",
+    ingredients: ["base", "crème", "champignon", "poivron"],
+    image: "/images/pizzaUne.jpg"
+  },
+  {
+    id: 4,
+    title: "Rafaello",
+    category: "blanche",
+    ingredients: ["base", "crème", "oignon", "saumon"],
+    image: "/images/pizzaDeux.jpg"
+  },
+  {
+    id: 5,
+    title: "Splinter",
+    category: "exotique",
+    ingredients: ["base", "chestnut", "poivron", "saumon"],
+    image: "/images/pizzaUne.jpg"
+  },
+  {
+    id: 6,
+    title: "April",
+    category: "exotique",
+    ingredients: ["base", "butternut", "jambon", "poivron"],
+    image: "/images/pizzaDeux.jpg"
+  }
+];
