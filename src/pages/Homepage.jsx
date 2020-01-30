416c5b8ad44430ebe262c072a3131704096b8c0b
Alice Thammavong <thammavongalice@gmail.com>
2020-01-30 11:41:25 +0100

    Homepage background

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import Background from "../components/ Background";
+import styled from "styled-components";
+
+const StyledHomepage = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  .text {
+    width: 60vh;
+    font-weight: bold;
+    font-size: 100px;
+  }
+`;
 
 function Homepage() {
   return (
-    <div>
-      <img id="logo" src="images/logo.png" alt=""></img>
-    </div>
+    <StyledHomepage>
+      <Background />
+      <div>
+        <h1 className="text">Street Food:</h1>
+        <p className="text">Pizza au coin de la rue</p>
+      </div>
+    </StyledHomepage>
   );
 }
 
